8a468fe6819d496d53ced3770ef03d6d42f87364
breadybread123 <2.74033632e+08+breadybread123@users.noreply.github.com>
2026-06-20 15:26:29 +0000

    Update: Aggressive thread blocking and immediate channel deletion

--- role-blocker/role-blocker.py
+++ role-blocker/role-blocker.py
@@ -7,49 +7,38 @@ class RoleBlocker(commands.Cog):
         self.blocked_role_id = 1480597515605770422 # The role ID to block users
 
     @commands.Cog.listener()
-    async def on_thread_ready(self, thread, creator, category, initial_message):
-        # This event is dispatched when a thread channel is created and the genesis_message is sent.
-        # It's a more reliable point to intervene if on_thread_initiate doesn't block.
-        
-        # Check if the creator (user) has the blocked role
-        guild = self.bot.get_guild(thread.guild_id)
-        if not guild:
+    async def on_message(self, message):
+        # Only process DMs to the bot
+        if message.guild is not None or message.author.bot:
             return
 
-        member = guild.get_member(creator.id)
-        if not member:
-            return
-
-        if discord.utils.get(member.roles, id=self.blocked_role_id):
-            # User has the blocked role, send an error message and close the thread immediately
-            error_embed = discord.Embed(
-                title="Thread Creation Blocked",
-                description="You are currently blacklisted from opening new support threads. If you believe this is an error, please contact a staff member directly.",
-                color=discord.Color.red()
-            )
-            try:
-                await creator.send(embed=error_embed)
-            except discord.Forbidden:
-                pass
-            
-            # Close the thread immediately
-            try:
-                # Based on Modmail's common structure, threads have a close method
-                # or can be closed via the bot's modmail component.
-                if hasattr(thread, 'close'):
-                    await thread.close(closer=self.bot.user, silent=True, delete_channel=True, message="Blacklisted user attempted to open a thread.")
-                elif hasattr(self.bot, 'modmail') and hasattr(self.bot.modmail, 'close_thread'):
-                    await self.bot.modmail.close_thread(thread.channel.id, closer=self.bot.user, silent=True)
-                else:
-                    # Fallback: if we can't close it programmatically, at least delete the channel if possible
-                    if hasattr(thread, 'channel') and thread.channel:
-                        await thread.channel.delete(reason="Blacklisted user.")
-            except Exception as e:
-                print(f"Error closing thread for blacklisted user: {e}")
+        # Fetch the member from the main guild to check roles
+        # We assume the bot is in at least one guild where this role exists
+        for guild in self.bot.guilds:
+            member = guild.get_member(message.author.id)
+            if member:
+                if discord.utils.get(member.roles, id=self.blocked_role_id):
+                    # User is blacklisted, send error and stop Modmail from seeing this message
+                    error_embed = discord.Embed(
+                        title="Access Denied",
+                        description="You are currently blacklisted from opening new support threads. If you believe this is an error, please contact a staff member directly.",
+                        color=discord.Color.red()
+                    )
+                    try:
+                        await message.author.send(embed=error_embed)
+                    except discord.Forbidden:
+                        pass
+                    
+                    # To stop Modmail from processing this message, we can't easily "cancel" the event
+                    # for other listeners in discord.py. However, many Modmail versions check if a 
+                    # message has already been handled or if a certain condition is met.
+                    # Since we can't easily block other listeners, let's also use the reactive approach
+                    # but more aggressively.
+                    return
 
     @commands.Cog.listener()
-    async def on_thread_initiate(self, thread, creator, category, initial_message):
-        # Keep this as a first line of defense, even if it's not 100% reliable in all Modmail versions
+    async def on_thread_ready(self, thread, creator, category, initial_message):
+        # Aggressive reactive closing
         guild = self.bot.get_guild(thread.guild_id)
         if not guild:
             return
@@ -59,9 +48,26 @@ class RoleBlocker(commands.Cog):
             return
 
         if discord.utils.get(member.roles, id=self.blocked_role_id):
-            # Try to raise an exception that Modmail might catch to stop creation
-            # Some versions of Modmail check for specific return values or exceptions here.
-            raise commands.CheckFailure("User is blacklisted.")
+            # Close and delete the channel immediately
+            try:
+                # 1. Send closing message to user
+                error_embed = discord.Embed(
+                    title="Thread Blocked",
+                    description="You are blacklisted. This thread is being closed automatically.",
+                    color=discord.Color.red()
+                )
+                await creator.send(embed=error_embed)
+
+                # 2. Delete the channel directly via discord.py API
+                # This is the most direct way to make it "disappear" from the staff view
+                if hasattr(thread, 'channel') and thread.channel:
+                    await thread.channel.delete(reason="Blacklisted user.")
+                
+                # 3. Try to call Modmail's internal close if possible
+                if hasattr(thread, 'close'):
+                    await thread.close(closer=self.bot.user, silent=True, delete_channel=True)
+            except Exception as e:
+                print(f"Aggressive close failed: {e}")
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(RoleBlocker(bot))
